4e1e7020087b88de915631a735af203f6524309d
fannskuyy <111479975+fannskuyy@users.noreply.github.com>
2022-08-18 00:01:00 -0700

    Add files via upload

---
+++ python2-Fauzi.py
@@ -0,0 +1,5 @@
+nama_depan = "uji"
+nama_belakang = "ifan fauzi"
+print("nama depan saya adalah",nama_depan)
+print("nama belakang saya adalah",nama_belakang)
+print("nama lengkap saya adalah",nama_depan,nama_belakang)
