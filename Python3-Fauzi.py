eb561256e8bb261a47172532fafe227ec1ae8443
fannskuyy <111479975+fannskuyy@users.noreply.github.com>
2022-08-21 22:05:54 -0700

    Add files via upload

---
+++ Python3-Fauzi.py
@@ -0,0 +1,15 @@
+nama_depan =input("masukan nama depan: ")
+nama_belakang =input("masuakan nama belakang: ")
+print("nama lengkap saya adalah",nama_depan,nama_belakang)
+
+a = int(input("masukan nilai A: "))
+b = float(input("masukan nilal B: "))
+c = a + b
+
+print("A =", a)
+print("B =", b)
+print(a,"+",b,"=", c)
+
+x = int(input("masukan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius = ", y, "fahreheit")
