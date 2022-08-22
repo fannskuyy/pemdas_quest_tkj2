e50c2ecf8a9b11824abb536d53f9b82e9326b801
fannskuyy <111479975+fannskuyy@users.noreply.github.com>
2022-08-21 22:01:18 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,17 @@
+print("halo semuanya")
+print("perkenalkan ")
+nama_depan =input("nama depan saya")
+nama_belakang =input("nama belakang saya")
+nama_lengkap =input("nama lengkap saya adalah")
+
+a = int(input("masukan nilai A: "))
+b = float(input("masukan nilai B: "))
+c = a + b
+
+print("A =", a)
+print("B =", b)
+print(a,"+",b,"=", c)
+
+x = int(input("masukan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius = ", y, "fahreheit")
